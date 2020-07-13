edbbc73a232e2b02cd808060bafe1b546972c226
Iván <ivanmzes@icloud.com>
2020-07-13 23:20:45 +0200

    initial core data changes observer

--- Wake on Lan/AppDelegate.swift
+++ Wake on Lan/AppDelegate.swift
@@ -17,9 +17,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var moc: NSManagedObjectContext!
     let statusBarMenu = NSMenu(title: "Status Bar Menu")
     
+    var notificationToken: NSObjectProtocol?
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
-        // Create the SwiftUI view that provides the window contents.
+        // Status Bar Item
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        statusBarItem.menu = statusBarMenu
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(systemSymbolName: "network", accessibilityDescription: "Wake on Lan")
+        }
+        
+        // Core Data Context
         let container = NSPersistentContainer(name: "WOL")
         container.loadPersistentStores { storeDescription, error in
             if error != nil {
@@ -28,51 +37,75 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
         moc = container.viewContext
         
-        // Create the status item
-        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
-        statusBarItem.menu = statusBarMenu
-        if let button = self.statusBarItem.button {
-            button.image = NSImage(systemSymbolName: "network", accessibilityDescription: "Wake on Lan")
-        }
-        
+        // Core Data Controller
         let fetchRequest = NSFetchRequest<WOLDevice>(entityName: "WOLDevice")
-        // Configure the request's entity, and optionally its predicate
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
-        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: nil, cacheName: nil)
+        
+        
+        let controller = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: moc,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
         controller.delegate = self
         
+        //
         do {
             try controller.performFetch()
-            let objects = (controller.fetchedObjects ?? []) as! [WOLDevice]
-            fillMenu(objects: objects)
-            print("performFetch")
+            let wolDevices = (controller.fetchedObjects ?? []) 
+            fillMenu(with: wolDevices)
         } catch {
             fatalError("Failed to fetch entities: \(error)")
         }
         
+        // Suscribe to Core Data changes when Context saves changes
+        notificationToken = NotificationCenter.default
+            .addObserver(forName: .NSManagedObjectContextDidSave,
+                         object: nil,
+                         queue: nil) { _ in
+                do {
+                    try controller.performFetch()
+                } catch {
+                    fatalError("Failed to fetch entities: \(error)")
+                }
+            }
         
         NSApp.activate(ignoringOtherApps: true)
     }
     
+    func fillMenu(with objects: [WOLDevice]){
+        
+        statusBarMenu.removeAllItems()
+        
+        objects.forEach { object in
+            let item = NSMenuItem(title: object.name,
+                                  action: #selector(AppDelegate.sendWOL(_:)),
+                                  keyEquivalent: "")
+            item.representedObject = object
+            statusBarMenu.addItem(item)
+        }
+        
+        statusBarMenu.addItem(.separator())
+        statusBarMenu.addItem(withTitle: "Preferences",
+                              action: #selector(AppDelegate.openPreferences),
+                              keyEquivalent: "")
+        statusBarMenu.addItem(withTitle: "Quit",
+                              action: #selector(AppDelegate.quitApp),
+                              keyEquivalent: "")
+    }
     
 }
 
-
-extension AppDelegate : NSFetchedResultsControllerDelegate {
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        let objects = (controller.fetchedObjects ?? []) as [WOLDevice]
-        print(objects)
-        fillMenu(objects: objects)
-        
-    }
-    
+// MARK: - Selectors
+extension AppDelegate {
     @objc func openPreferences(_ sender: AnyObject?){
         let window = AKPreferencesWindow()
         window.makeKeyAndOrderFront(nil)
         window.isReleasedWhenClosed = false
     }
     
-    @objc func quitWOL(_ sender: AnyObject?){
+    @objc func quitApp(_ sender: AnyObject?){
         NSApplication.shared.terminate(self)
     }
     
@@ -80,22 +113,14 @@ extension AppDelegate : NSFetchedResultsControllerDelegate {
         let wolDevice = sender?.representedObject as! WOLDevice
         wolDevice.awake()
     }
+}
+
+// MARK: - NSFetchedResultsControllerDelegate
+extension AppDelegate : NSFetchedResultsControllerDelegate {
     
-    func fillMenu(objects: [WOLDevice]){
-        statusBarMenu.removeAllItems()
-        objects.forEach { object in
-            
-            let item = NSMenuItem(title: object.name, action: #selector(AppDelegate.sendWOL(_:)), keyEquivalent: "")
-            item.representedObject = object
-            statusBarMenu.addItem(item)
-        }
-        statusBarMenu.addItem(.separator())
-        statusBarMenu.addItem(withTitle: "Preferences",
-                              action: #selector(AppDelegate.openPreferences),
-                              keyEquivalent: "")
-        statusBarMenu.addItem(withTitle: "Quit",
-                              action: #selector(AppDelegate.quitWOL),
-                              keyEquivalent: "")
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        let wolDevices = (controller.fetchedObjects ?? []) as! [WOLDevice]
+        fillMenu(with: wolDevices)
     }
     
 }
