35bfb7e1cc9913e165642dec52c8fb40c5f856be
Iván Moreno Zambudio <ivanmzes@icloud.com>
2020-07-14 20:32:23 +0200

    fix device view hover on preferences

--- Wake on Lan/SwiftUI/DeviceView.swift
+++ Wake on Lan/SwiftUI/DeviceView.swift
@@ -10,9 +10,6 @@ import SwiftUI
 struct DeviceView: View {
     
     @ObservedObject var device: WOLDevice
-    @State var enableHover = false
-    @State private var hover = false
-    
     
     public var body: some View {
         HStack {
@@ -26,12 +23,7 @@ struct DeviceView: View {
         }
         .padding(8)
         .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-        .background(hover && enableHover ? Color(NSColor.controlAccentColor) : Color.white.opacity(0))
-        .foregroundColor(hover && enableHover ? Color.white : Color(NSColor.textColor))
         .cornerRadius(8)
-        .onHover { hovering in
-            self.hover = hovering
-        }
         .contextMenu {
             Button(action:{device.awake()}){
                 Text("Send WOL")
